ab43e1d6ce349b5286e8922051f8850ec1c5968f
SebastianHajdus <sebastian.hajdus@gmail.com>
2016-12-15 21:03:17 +0100

    work on slider

--- js/app.js
+++ js/app.js
@@ -2,5 +2,20 @@ $(function () {
 
 console.log('dziala');
 
+    var next = document.querySelector('#nextPicture');
+    console.log(next);
+
+    var prev = document.querySelector('#prevPicture');
+    console.log(prev);
+
+    var list = document.querySelectorAll('li');
+    console.log(list);
+
+    var index = 0;
+
+    list[index].classList.add('visible');
+
+
+    
 
 });
